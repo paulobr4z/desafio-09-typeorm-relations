d19d48fc43b0f284a2bbd01661addb8bafbd1f2a
Paulo Braz <paulobrazaraujo@gmail.com>
2021-02-20 15:55:19 -0300

    challenge finalized

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,16 +6,23 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
 export default Customer;
+ 
